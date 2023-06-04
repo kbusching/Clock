a305e63051756571e0e7a6d46416ced4891b778f
Katie Busching <katie.busching@gmail.com>
2023-06-04 10:23:34 -0400

    New Clock

--- main.js
+++ main.js
@@ -7,8 +7,43 @@ let clock = {
     setSeconds: function (s) {this.seconds = s;}
 };
 
+//Dates and Times
 let now = new Date ()
 clock.setHours(now.getHours());
 clock.setMinutes(now.getMinutes());
 clock.setSeconds(now.getSeconds());
 console.log(clock)
+
+function formatTime(time){
+    return time < 10 ? '0' + time: time;
+}
+
+//Intervals
+setInterval(function(){
+    clock.seconds++;
+    if(clock.seconds ===60) {
+        clock.seconds = 0;
+        clock.minutes++;
+    }
+    if(clock.minutes === 60) {
+        clock.minutes = 0;
+        clock.hours++;
+    }
+    if(clock.hours === 24) {
+        clock.hours = 0;
+    }
+    document.getElementById('clock').innerHTML = formatTime(clock.hours) + ":" +formatTime(clock.minutes) + ":" + formatTime(clock.seconds)
+}, 1000);
+
+document.getElementById('remove').onclick = function(){
+    let element = document.getElementById("clock");
+    element.parentNode.removeChild(element);
+}
+
+let clockElement = document.querySelector("#clock");
+
+document.getElementById('reset').onclick = function(){
+    clock.hours = 0;
+    clock.minutes = 0;
+    clock.seconds = 0;
+};
